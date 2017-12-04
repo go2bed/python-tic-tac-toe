d6ecf954cff12a095a8fa4f4e466a0f8f58738a5
Andrey_Chadov <androusj@mail.ru>
2017-12-04 21:56:16 +0600

    [IMP] Defined additional functions

--- com.go2bed/Game.py
+++ com.go2bed/Game.py
@@ -1,12 +1,35 @@
 game_is_over = False
 
-board = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def play_the_game():
+    print("the board is ")
+    display_board(board)
+    player_choice = player_input()
+    print("your choice is: " + player_choice)
+    win_check()
+    pass
+
+
+def display_board(board):
+    for i in board:
+        if i % 3 == 0:
+            print(i, end="\n")
+        else:
+            print(i, end="|")
+    pass
+
+
+def player_input():
+    s = input("your turn, select the number")
+    return s
+
+def place_marker(board, marker, position):
+    pass
+
+
+def win_check(board, mark):
     global game_is_over
-    print(board)
-    s = input("select your")
     game_is_over = True
     pass
 
